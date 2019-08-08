e8856e93f176f1f3fcae8f13cdf8b52e74563516
Damian Wójcik <damian.wojcik@agora.pl>
2019-08-08 13:20:55 +0200

    removed content placeholders

--- pages/index.vue
+++ pages/index.vue
@@ -9,37 +9,6 @@
         </div>
       </div>
       <!-- /.tile is-ancestor -->
-      <div class="tile is-ancestor">
-        <div class="tile is-6 is-vertical is-parent">
-          <div class="tile is-child box">
-            <p class="title">Table 1</p>
-          </div>
-        </div>
-        <div class="tile is-6 is-vertical is-parent">
-          <div class="tile is-child box">
-            <p class="title">Table 2</p>
-          </div>
-        </div>
-      </div>
-      <!-- /.tile is-ancestor -->
-      <div class="tile is-ancestor">
-        <div class="tile is-4 is-vertical is-parent">
-          <div class="tile is-child box">
-            <p class="title">Box 1</p>
-          </div>
-        </div>
-        <div class="tile is-4 is-vertical is-parent">
-          <div class="tile is-child box">
-            <p class="title">Box 2</p>
-          </div>
-        </div>
-        <div class="tile is-4 is-vertical is-parent">
-          <div class="tile is-child box">
-            <p class="title">Box 3</p>
-          </div>
-        </div>
-      </div>
-      <!-- /.tile is-ancestor -->
     </div>
     <!-- /.container -->
   </section>
